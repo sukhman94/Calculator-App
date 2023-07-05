34bd697128d5271552642fa330931eb322f695c3
Sukhmandeep Kaur <randhawa.sukhman94@gmail.com>
2023-07-05 10:44:30 -0400

    updated functions

--- script.js
+++ script.js
@@ -14,23 +14,23 @@ function add(){
 function subtract(){
     let a = Number(document.getElementById("num1-el").value);
     let b = Number(document.getElementById("num2-el").value);
-    sum1 = a - b;
-   document.getElementById("sum1").textContent= "subtraction: " + sum1 ;
+   let sub = a - b;
+   document.getElementById("sum1").textContent= "subtraction: " + sub ;
 
 }
 function divide(){
     let a = Number(document.getElementById("num1-el").value);
     let b = Number(document.getElementById("num2-el").value);
-    sum1 = a/b;
-   document.getElementById("sum1").textContent= "Division: " + sum1 ;
+    let div = a/b;
+   document.getElementById("sum1").textContent= "Division: " + div ;
 
 }
 
 function multiply(){
     let a = Number(document.getElementById("num1-el").value);
     let b = Number(document.getElementById("num2-el").value);
-    sum1 = a*b;
-   document.getElementById("sum1").textContent= "Multiplication: " + sum1 ;
+    let mul = a*b;
+   document.getElementById("sum1").textContent= "Multiplication: " + mul ;
 
 }
 
